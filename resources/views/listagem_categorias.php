d62df6b35d3561331cd6e68a1b009426156389af
PATRICIA\Patricia <patriciasoramos@gmail.com>
2018-01-28 12:31:36 -0200

    no message

--- resources/views/listagem_categorias.php
+++ resources/views/listagem_categorias.php
@@ -3,7 +3,7 @@
 		<link href="/DesafioProdutos/public/css/custom.css" rel="stylesheet">
 		<link href="/DesafioProdutos/public/css/bootstrap.min.css" rel="stylesheet">
 		<link href="/DesafioProdutos/public/js/bootstrap.min.js" rel="stylesheet">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	</head>
 	<body>
 	<div class="col-lg-8">
